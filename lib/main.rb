d180cf0e96c4d06bb505b9b44f94e7887ccbd23e
David Gasana Manzi <gmdavid59@gmail.com>
2021-05-07 13:32:35 +0200

    Co-authored-by: Li-Hsuan Lung <lihsuanlung@gmail.com>

--- lib/main.rb
+++ lib/main.rb
@@ -1,31 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'David_tic_tac_toe/version'
-require_relative 'David_tic_tac_toe/board'
-require_relative 'David_tic_tac_toe/player'
-require_relative 'David_tic_tac_toe/game'
+require_relative 'David_tic_tac_toe'
 
-module DavidTicTacToe
-  @game = DavidTicTacToe::Game.new(DavidTicTacToe::Board.new, DavidTicTacToe::Player.new('David', 'X'),
-                                   DavidTicTacToe::Player.new('Manzi', 'O'))
-  def self.play
-    current_player = @game.player1
-    while true
-      @game.game_board.print_board
-      puts "------------------\n\nPlayer #{current_player.name}'s turn: "
-      input = gets.chomp
-      indices = input.split(' ', 2)
-      indices = indices.map(&:to_i)
-      next unless @game.game_board.valid_position(indices)
-
-      @game.game_board.add_play_to_board(indices, current_player.marker)
-      return "#{current_player.name} has Won!!!!!!!" if @game.won_the_game(current_player.marker)
-      return "No winner! TIED!!!!!!" if @game.game_board.board_full
-
-      current_player = @game.switch_players(current_player)
-
-    end
-  end
-
-  puts play
-end
+puts DavidTicTacToe.play
--- spec/David_tic_tac_toe/game_spec.rb
+++ spec/David_tic_tac_toe/game_spec.rb
@@ -13,18 +13,32 @@ RSpec.describe DavidTicTacToe::Game do
     expect(game.player1.name).to eq 'David'
   end
 
-  it "should return true if player has won" do
-    game.game_board.add_play_to_board([0, 2], 'X')
-    game.game_board.add_play_to_board([1, 1], 'X')
-    game.game_board.add_play_to_board([2, 0], 'X')
-    expect(game.won_the_game('X')).to eq true
-  end
+  describe '#won_the_game' do
+    context 'when the game is won' do
+      [
+        [[[1, 2], [1, 1], [1, 0]], 'O', 'ROW_WISE'],
+        [[[0, 0], [1, 0], [2, 0]], 'X', 'COLUMN_WISE'],
+        [[[0, 0], [1, 1], [2, 2]], 'O', 'MAIN_DIAGONAL_WISE'],
+        [[[0, 2], [1, 1], [2, 0]], 'X', 'COUNTER_DIAGONAL_WISE']
+      ].each do |moves, mark, description|
+        it "should return true if player has won (#{description})" do
+          moves.each do |move|
+            game.game_board.add_play_to_board(move, mark)
+          end
 
-  it "should return false if the player hasn't won" do
-    game.game_board.add_play_to_board([0, 2], 'X')
-    game.game_board.add_play_to_board([1, 1], 'O')
-    game.game_board.add_play_to_board([2, 0], 'X')
-    expect(game.won_the_game('X')).to eq false
+          expect(game.won_the_game(mark)).to eq true
+        end
+      end
+    end
+
+    context 'when the game is lost' do
+      it "should return false if the player hasn't won" do
+        game.game_board.add_play_to_board([0, 0], 'X')
+        game.game_board.add_play_to_board([1, 1], 'O')
+        game.game_board.add_play_to_board([2, 0], 'X')
+        expect(game.won_the_game('X')).to eq false
+      end
+    end
   end
 
   it "should switch the players" do
